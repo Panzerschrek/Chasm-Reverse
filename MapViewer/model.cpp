f9206d6423cb38583135c9feb460fd5291fa5f6d
Panzerschrek <AntiPanzerschrek@yandex.ru>
2016-10-14 16:54:29 +0700

    Sdelana zagruzka podmodelej iz .car formata.

--- MapViewer/model.cpp
+++ MapViewer/model.cpp
@@ -72,8 +72,8 @@ void LoadModel_o3( const Vfs::FileContent& model_file, const Vfs::FileContent& a
 	out_model.vertices.clear();
 	out_model.regular_triangles_indeces.clear();
 	out_model.transparent_triangles_indeces.clear();
-	out_model.submodels.clear();
 	out_model.texture_data.clear();
+	out_model.submodels.clear();
 
 	unsigned short polygon_count;
 	unsigned short vertex_count;
@@ -206,7 +206,10 @@ void LoadModel_car( const Vfs::FileContent& model_file, Model& out_model )
 		texture_texels );
 
 	out_model.frame_count= 0u;
-	const unsigned short* const animations= reinterpret_cast<const unsigned short*>( model_file.data() + 0x00u );
+	const unsigned short* const animations=
+		reinterpret_cast<const unsigned short*>( model_file.data() + 0x00u );
+	const unsigned short* const submodels_animations=
+		reinterpret_cast<const unsigned short*>( model_file.data() + 0x28u );
 	for( unsigned int i= 0u; i < 20u; i++ )
 	{
 		const unsigned int animation_frame_count= animations[i] / ( sizeof(Vertex_o3) * vertex_count );
@@ -222,87 +225,140 @@ void LoadModel_car( const Vfs::FileContent& model_file, Model& out_model )
 		out_model.frame_count+= animation_frame_count;
 	}
 
-	const Vertex_o3* vertices=
-		reinterpret_cast<const Vertex_o3*>( model_file.data() + c_textures_offset + texture_texels );
-
-	const Polygon_o3* const polygons= reinterpret_cast<const Polygon_o3*>( model_file.data() + 0x66u );
-
-	const unsigned int bytes_left=
-			model_file.size() - (
-			c_textures_offset +
-			out_model.texture_data.size() +
-			out_model.frame_count * sizeof(Vertex_o3) * vertex_count );
-
-	std::cout << "Bytes left: " << int(bytes_left)  << std::endl;
-
-	std::vector<Model::Vertex> tmp_vertices;
-	unsigned int current_vertex_index= 0u;
-
-	for( unsigned int p= 0u; p < polygon_count; p++ )
+	const auto prepare_model=
+	[&out_model](
+		const unsigned int vertex_count, const unsigned int polygon_count,
+		const Vertex_o3* const vertices, const Polygon_o3* const polygons,
+		Submodel& out_submodel )
 	{
-		const Polygon_o3& polygon= polygons[p];
-		const bool polygon_is_triangle= polygon.vertices_indeces[3u] >= vertex_count;
-		const bool polygon_is_twosided= ( polygon.flags & Polygon_o3::Flags::Twosided ) != 0u;
+		std::vector<Model::Vertex> tmp_vertices;
+		unsigned int current_vertex_index= 0u;
 
-		const unsigned int polygon_vertex_count= polygon_is_triangle ? 3u : 4u;
-		unsigned int polygon_index_count= polygon_is_triangle ? 3u : 6u;
-		if( polygon_is_twosided ) polygon_index_count*= 2u;
-
-		const unsigned int first_vertex_index= current_vertex_index;
-		tmp_vertices.resize( tmp_vertices.size() + polygon_vertex_count * out_model.frame_count );
-		Model::Vertex* v= tmp_vertices.data() + first_vertex_index * out_model.frame_count;
-
-		for( unsigned int j= 0u; j < polygon_vertex_count; j++ )
+		for( unsigned int p= 0u; p < polygon_count; p++ )
 		{
-			for( unsigned int frame= 0u; frame < out_model.frame_count; frame++ )
+			const Polygon_o3& polygon= polygons[p];
+			const bool polygon_is_triangle= polygon.vertices_indeces[3u] >= vertex_count;
+			const bool polygon_is_twosided= ( polygon.flags & Polygon_o3::Flags::Twosided ) != 0u;
+
+			const unsigned int polygon_vertex_count= polygon_is_triangle ? 3u : 4u;
+			unsigned int polygon_index_count= polygon_is_triangle ? 3u : 6u;
+			if( polygon_is_twosided ) polygon_index_count*= 2u;
+
+			const unsigned int first_vertex_index= current_vertex_index;
+			tmp_vertices.resize( tmp_vertices.size() + polygon_vertex_count * out_submodel.frame_count );
+			Model::Vertex* v= tmp_vertices.data() + first_vertex_index * out_submodel.frame_count;
+
+			for( unsigned int j= 0u; j < polygon_vertex_count; j++ )
 			{
-				Model::Vertex& vertex= v[ frame + j * out_model.frame_count ];
-				const Vertex_o3& in_vertex= vertices[ polygon.vertices_indeces[j] + frame * vertex_count ];
+				for( unsigned int frame= 0u; frame < out_submodel.frame_count; frame++ )
+				{
+					Model::Vertex& vertex= v[ frame + j * out_submodel.frame_count ];
+					const Vertex_o3& in_vertex= vertices[ polygon.vertices_indeces[j] + frame * vertex_count ];
 
-				vertex.tex_coord[0]= float( polygon.uv[j][0]) / float( out_model.texture_size[0] << 8u );
-				vertex.tex_coord[1]= float( polygon.uv[j][1] + 4u * polygon.v_offset ) / float( out_model.texture_size[1] << 8u );
+					vertex.tex_coord[0]= float( polygon.uv[j][0]) / float( out_model.texture_size[0] << 8u );
+					vertex.tex_coord[1]= float( polygon.uv[j][1] + 4u * polygon.v_offset ) / float( out_model.texture_size[1] << 8u );
 
-				for( unsigned int c= 0u; c < 3u; c++ )
-					vertex.pos[c]= float( in_vertex.xyz[c] ) * g_3o_model_coords_scale;
+					for( unsigned int c= 0u; c < 3u; c++ )
+						vertex.pos[c]= float( in_vertex.xyz[c] ) * g_3o_model_coords_scale;
+				}
 			}
-		}
 
-		auto& dst_indeces=
-			(polygon.flags & Polygon_o3::Flags::Translucent ) == 0u
-				? out_model.regular_triangles_indeces
-				: out_model.transparent_triangles_indeces;
+			auto& dst_indeces=
+				(polygon.flags & Polygon_o3::Flags::Translucent ) == 0u
+					? out_submodel.regular_triangles_indeces
+					: out_submodel.transparent_triangles_indeces;
 
-		dst_indeces.resize( dst_indeces.size() + polygon_index_count );
-		unsigned short* ind= dst_indeces.data() + dst_indeces.size() - polygon_index_count;
+			dst_indeces.resize( dst_indeces.size() + polygon_index_count );
+			unsigned short* ind= dst_indeces.data() + dst_indeces.size() - polygon_index_count;
 
-		ind[0u]= first_vertex_index + 2u;
-		ind[1u]= first_vertex_index + 1u;
-		ind[2u]= first_vertex_index + 0u;
-		if( !polygon_is_triangle )
-		{
-			ind[3u]= first_vertex_index + 0u;
-			ind[4u]= first_vertex_index + 3u;
-			ind[5u]= first_vertex_index + 2u;
-		}
-
-		if( polygon_is_twosided )
-		{
-			ind+= polygon_index_count >> 1u;
-			ind[0u]= first_vertex_index + 0u;
+			ind[0u]= first_vertex_index + 2u;
 			ind[1u]= first_vertex_index + 1u;
-			ind[2u]= first_vertex_index + 2u;
+			ind[2u]= first_vertex_index + 0u;
 			if( !polygon_is_triangle )
 			{
-				ind[3u]= first_vertex_index + 2u;
+				ind[3u]= first_vertex_index + 0u;
 				ind[4u]= first_vertex_index + 3u;
-				ind[5u]= first_vertex_index + 0u;
+				ind[5u]= first_vertex_index + 2u;
 			}
+
+			if( polygon_is_twosided )
+			{
+				ind+= polygon_index_count >> 1u;
+				ind[0u]= first_vertex_index + 0u;
+				ind[1u]= first_vertex_index + 1u;
+				ind[2u]= first_vertex_index + 2u;
+				if( !polygon_is_triangle )
+				{
+					ind[3u]= first_vertex_index + 2u;
+					ind[4u]= first_vertex_index + 3u;
+					ind[5u]= first_vertex_index + 0u;
+				}
+			}
+
+			current_vertex_index+= polygon_vertex_count;
+		} // for polygons
+
+		SwapVertexOrder( tmp_vertices, out_submodel.frame_count, out_submodel.vertices );
+	};
+
+	{ // Main model
+		const Vertex_o3* vertices=
+			reinterpret_cast<const Vertex_o3*>( model_file.data() + c_textures_offset + texture_texels );
+		const Polygon_o3* const polygons=
+			reinterpret_cast<const Polygon_o3*>( model_file.data() + 0x66u );
+
+		prepare_model( vertex_count, polygon_count, vertices, polygons, out_model );
+	}
+
+	unsigned int submodels_offset=
+		c_textures_offset +
+		out_model.texture_data.size() +
+		out_model.frame_count * sizeof(Vertex_o3) * vertex_count;
+
+	for( unsigned int i= 0u; i < 3u; i++ )
+	{
+		const unsigned int c_animation_data_offset= 0x4806u;
+
+		const unsigned int submodel_animation_data_size=
+			submodels_animations[ i * 2u ] + submodels_animations[ i * 2u + 1u ];
+
+		if( submodel_animation_data_size == 0u )
+			continue;
+
+		out_model.submodels.emplace_back();
+		Submodel& submodel= out_model.submodels.back();
+
+		std::memcpy( &vertex_count, model_file.data() + submodels_offset + 0x4800u, sizeof(unsigned short) );
+		std::memcpy( &polygon_count, model_file.data() + submodels_offset + 0x4802u, sizeof(unsigned short) );
+
+		submodel.frame_count= submodel_animation_data_size / ( sizeof(Vertex_o3) * vertex_count );
+
+		const Vertex_o3* vertices=
+			reinterpret_cast<const Vertex_o3*>( model_file.data() + submodels_offset + c_animation_data_offset );
+		const Polygon_o3* const polygons=
+			reinterpret_cast<const Polygon_o3*>( model_file.data() + submodels_offset );
+
+		prepare_model( vertex_count, polygon_count, vertices, polygons, submodel );
+
+		// Setup animations.
+		// Each submodel have up to 2 animations.
+		for( unsigned int a= 0u; a < 2u; a++ )
+		{
+			const unsigned int animation_frame_count=
+				submodels_animations[ i * 2u + a ] / ( sizeof(Vertex_o3) * vertex_count );
+			if( animation_frame_count == 0u )
+				continue;
+
+			submodel.animations.emplace_back();
+			Submodel::Animation& anim= submodel.animations.back();
+
+			anim.id= i;
+			anim.first_frame= submodel.frame_count;
+			anim.frame_count= animation_frame_count;
 		}
 
-		current_vertex_index+= polygon_vertex_count;
-	} // for polygons
-
-	SwapVertexOrder( tmp_vertices, out_model.frame_count, out_model.vertices );
+		submodels_offset+= c_animation_data_offset + submodel_animation_data_size;
+	} // for submodels
 }
 
 } // namespace ChasmReverse
