91eecd5bc7b5eeadafd63556a36e8441ea3db332
Panzerschrek <AntiPanzerschrek@yandex.ru>
2017-05-08 21:03:44 +0700

    Ispravlenije predupreždenij sborki.

--- Common/files.cpp
+++ Common/files.cpp
@@ -9,7 +9,7 @@ void FileRead( std::FILE* const file, void* buffer, const unsigned int size )
 
 	do
 	{
-		const int read= std::fread( buffer + read_total, 1, size - read_total, file );
+		const int read= std::fread( static_cast<char*>(buffer) + read_total, 1, size - read_total, file );
 		if( read <= 0 )
 			break;
 
@@ -23,7 +23,7 @@ void FileWrite( std::FILE* const file, const void* buffer, const unsigned int si
 
 	do
 	{
-		const int write= std::fwrite( buffer + write_total, 1, size - write_total, file );
+		const int write= std::fwrite( static_cast<const char*>(buffer) + write_total, 1, size - write_total, file );
 		if( write <= 0 )
 			break;
 
