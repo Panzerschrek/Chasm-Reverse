f61824130afc701f8fa4d0b723ab8dd132ef3a2b
Panzerschrek <AntiPanzerschrek@yandex.ru>
2017-06-25 12:44:26 +0700

    Pri vyhode iz igry v konsolj pecatajetsä tekst s avtorami originaljnoj igry.

--- PanzerChasm/main.cpp
+++ PanzerChasm/main.cpp
@@ -1,6 +1,7 @@
 // main.cpp - entry point
 
 #include <memory>
+#include <iostream>
 
 #include <SDL.h>
 
@@ -20,5 +21,31 @@ extern "C" int main( int argc, char *argv[] )
 	{
 	}
 
+	// Print exit text to console.
+	// Authors list is reconstructed. In original game it was hardcoded in executable.
+	//TODO - maybe make system window with this text?
+	static const char c_exit_screen_text[]=
+R"(
+                                  PanzerChasm
+                    (c) 2016-2017 Artjom "Panzerschrek" Kunz
+--------------------------------------------------------------------------------
+                                CHASM - The Rift
+                      (C) Megamedia Corp, Action Forms Ltd.
+                          Published by Megamedia Corp.
+                         Developed by Action Forms Ltd.
+--------------------------------------------------------------------------------
+               Programming      :  Oleg Slusar
+               Artwork          :  Yaroslav Kravchenko,
+                                   Alexey Serhiy,
+                                   Alexey Pivtorak
+               SFX and Music    :  Alex Kot
+               Level Design     :  Yaroslav Kravchenko, Denis Vereschagin
+                                   Andrey Sharanevitch, Alexey Pechenkin
+               Directed by      :  Igor Karev, Denis Vereschagin
+               Special thanks to:  Alexander Soroka, Peter M.Kolos
+--------------------------------------------------------------------------------
+)";
+
+	std::cout << c_exit_screen_text;
 	return 0;
 }
