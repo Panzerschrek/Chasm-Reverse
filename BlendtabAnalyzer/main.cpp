bbe673b7f8f0a7b50e0a1ceec8b0e30d3535d34d
Panzerschrek <AntiPanzerschrek@yandex.ru>
2017-08-05 11:59:25 +0700

    Analizator tabliçy smešivanija vydajot sredneje znacenije aljfa-kanala.

--- BlendtabAnalyzer/main.cpp
+++ BlendtabAnalyzer/main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
@@ -41,6 +42,36 @@ static void ConvertBlendTable( const char* const in_table_file, const char* cons
 		extended_blend_tab[ x + c_color_width + y * ( 256u + c_color_width ) ]= x;
 
 	WriteTGA( 256u + c_color_width, 255u + c_color_width, extended_blend_tab, palette.data(), out_tga_file );
+
+	float average_alpha= 0.0f;
+	unsigned int total_nonzero_colors= 0u;
+	for( unsigned int y= 0u; y < 255u; y++ )
+	for( unsigned int x= 0u; x < 256u; x++ )
+	{
+		const unsigned int color_index= blend_tab[ x + y * 256u ];
+
+		unsigned int nonzero_components= 0u;
+		float average_components_alpha= 0.0f;
+		for( unsigned int c= 0u; c < 3u; c++ )
+		{
+			const float d0= float( palette[x*3u+c] ) - float( palette[y*3u+c] );
+			const float d1= float(palette[color_index*3u+c]) - float( palette[y*3u+c] );
+			const float c_threshold= 6.0f;
+			if( std::abs(d0) <= c_threshold || std::abs(d1) <= c_threshold )
+				continue;
+
+			nonzero_components++;
+			average_components_alpha+= d1 / d0;
+		}
+		if( nonzero_components == 0u )
+			continue;
+
+		total_nonzero_colors++;
+		const float alpha= average_components_alpha / float(nonzero_components);
+		average_alpha+= alpha;
+	}
+
+	std::cout << "Average alpha for table " << in_table_file << " is " << average_alpha / float(total_nonzero_colors) << std::endl;
 }
 
 int main()
