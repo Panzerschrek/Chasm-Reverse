5c86c5ae4bc6b687a28d7017d9f87f6aee66a34a
Jon Daniel <jondaniel879@gmail.com>
2023-04-08 01:19:43 +0200

    move panze_ogl_lib to src

--- src/panzer_ogl_lib/func_declarations.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define PROCESS_OGL_FUNCTION( TYPE, NAME ) extern TYPE NAME
-#include "functions_list.h"
-#undef PROCESS_OGL_FUNCTION
-
-void GetGLFunctions(
-	void* (*GetProcAddressFunc)(const char* func_name),
-	void (*FuncNotFoundCallback)(const char* func_name) = nullptr );
